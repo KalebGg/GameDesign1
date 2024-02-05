4b699f48fde4a9cb13368ede792d9e1dd7616759
KalebGg <Gayne.k@stu.janesville.k12.wi.us>
2024-02-05 13:43:51 -0600

    created and started work on 120prog, not done

---
+++ prog120a.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
