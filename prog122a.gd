f7cd31c49957a95c33bd09adf0c54243ff4433ea
KalebGg <Gayne.k@stu.janesville.k12.wi.us>
2024-02-05 13:26:11 -0600

    (done in class insruction)
    prog122 done, prog115 done

---
+++ prog122a.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_btn_calc_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_clear_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_exit_pressed():
+	pass # Replace with function body.
