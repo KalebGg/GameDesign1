4240cb5dee8884e4fa9f1c2e4dd9ab5f306620eb
KalebGg <Gayne.k@stu.janesville.k12.wi.us>
2024-02-20 13:43:49 -0600

    added prop

--- plt_world1.gd
+++ plt_world1.gd
@@ -8,3 +8,7 @@ func _ready():
 func _on_interact_mouse_entered():
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		$interact.queue_free()
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
